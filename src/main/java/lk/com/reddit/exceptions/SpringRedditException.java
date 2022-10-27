2e96e4810ac1c5c7a0901856a021cb0a2df90550
achintha98 <kalanaachintha@gmail.com>
2022-10-27 18:26:47 +0530

    Implemented email client

---
+++ src/main/java/lk/com/reddit/exceptions/SpringRedditException.java
@@ -0,0 +1,11 @@
+package lk.com.reddit.exceptions;
+
+/**
+ * @author Achintha Kalunayaka
+ * @since 10/27/2022
+ */
+public class SpringRedditException extends RuntimeException{
+    public SpringRedditException(String exception) {
+        super(exception);
+    }
+}
